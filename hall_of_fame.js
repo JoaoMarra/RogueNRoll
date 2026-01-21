0acd057d0b06602c63f8c327a99035b79c572f13
João Gabriel Silva Marra <silvamarraster@gmail.com>
2026-01-21 11:01:32 -0300

    fix ranking

--- hall_of_fame.js
+++ hall_of_fame.js
@@ -3,35 +3,54 @@ const hallOfFame = document.getElementById("hall_of_fame");
 const rankingContent = document.getElementById("ranking_content");
 
 async function buildRanking() {
-	const snapshot = await getRanking();
-
-	const ranking = [];
-	snapshot.forEach(c => ranking.push(c.val()));
-
-	ranking.reverse();
-	let index = 1;
-
-	let html = '<div style="display:flex;flex-direction:column">';
-	for(const r of ranking) {
-		const characters = r.characters.split("_");
-		html += `<div class="ranking_row" style="display:flex; align-items:top;margin-bottom:10px;">
-		<h1>${index}</h1>
-		<div style="font-size:30px;margin-left:20px;width:100%">
-		<div style="margin-left:auto">${formatDate(r.time)}</div>
-		<div>${r.name}</div>
-		<div style="color:#faefb6;font-size:40px">${r.score}</div>
-		<div style="margin-left:auto">`;
-		for(const c of characters) {
-			html += `<img src="sprites/${CHARACTERS[c].sprite}">`;
+	const snapshot = (await getRanking()).val();
+	if(snapshot != null) {
+		const ranking = [];
+		for(const u of Object.keys(snapshot)) {
+			ranking.push({...snapshot[u],'user':u});
 		}
-		html += `</div></div>
-	</div>`;
-	index++;
+		ranking.sort((a,b) => b.score - a.score);
+
+		let selfRank = await selfRanking();
+		let index = 1;
+
+		let html = '<div style="display:flex;flex-direction:column">';
+		let color = '#fff';
+		for(const r of ranking) {
+			if(selfRank != null && r.user == selfRank.user) {
+				color = '#00bbd4';
+				selfRank = null;
+			} else {
+				color = '#fff';
+			}
+			html += rankData(r, color, index);
+			index++;
+		}
+		if(selfRank != null) {
+			html += rankData(selfRank, '#00bbd4', index);
+		}
+		html += '</div>';
+		rankingContent.innerHTML = html;
+	}
 }
-html += '</div>';
 
-rankingContent.innerHTML = html;
+function rankData(r,color,index) {
+	const characters = r.characters.split("_");
+	let html = `<div class="ranking_row" style="display:flex; align-items:start;margin-bottom:10px;color:${color};">
+			<div style="font-size:40px;">${index}</div>
+			<div style="font-size:30px;margin-left:20px;width:100%">
+	<div style="display:flex;align-items:end;">
+			<div>${r.name}</div>
+			<div style="margin-left:auto;font-size:15px;">${formatDate(r.time)}</div>
+	</div>
+			<div style="color:#faefb6;font-size:40px">${r.score}</div>
+	<div style="margin-left:auto">`;
+	for(const c of characters) {
+		html += `<img src="sprites/${CHARACTERS[c].sprite}">`;
+	}
+	html += `</div></div></div>`
 
+	return html;
 }
 
 async function openRanking() {
